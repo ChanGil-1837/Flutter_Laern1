690cc87094b329560c770e98a25bdf6a5157a667
ChanGil <chan.gil1837@gmail.com>
2024-03-11 18:18:48 +0900

    2024-03-11, Stop Meal-App

---
+++ meals/lib/screens/tabs.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+import 'package:meals/screens/categories.dart';
+import 'package:meals/screens/meals.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() {
+    return _TabsScreenState();
+  }
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+  final List<Meal> _favoriteMeals = [];
+
+  void _showInfoMessage(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  void _toggleMealFavoriteStatus(Meal meal) {
+    final isExist = _favoriteMeals.contains(meal);
+
+    if (isExist) {
+      setState(() {
+        _showInfoMessage("Meal is no longer a favorite");
+        _favoriteMeals.remove(meal);
+      });
+    } else {
+      setState(() {
+        _showInfoMessage("Marked as a favorite");
+        _favoriteMeals.add(meal);
+      });
+    }
+  }
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage = CategoriesScreen(
+      onToggleFavorite: _toggleMealFavoriteStatus,
+    );
+    var activePageTitle = "categories";
+    if (_selectedPageIndex == 1) {
+      activePage = MealsScreen(
+          title: "Favorites",
+          onToggleFavorite: _toggleMealFavoriteStatus,
+          meals: _favoriteMeals);
+      activePageTitle = "Favorites";
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(activePageTitle),
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.set_meal), label: "Categories"),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: "Favorites")
+        ],
+      ),
+    );
+  }
+}
